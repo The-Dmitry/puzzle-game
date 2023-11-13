3598143fda3b0423e31346297d83f46f47545620
the-dmitry <dzmitrygaponenko@gmail.com>
2023-11-13 19:29:03 +0300

    feat: add message

--- coffee-house/src/home/home.js
+++ coffee-house/src/home/home.js
@@ -5,3 +5,5 @@ import BurgerView from '../js/burger/Burger';
 // eslint-disable-next-line no-unused-vars
 const slider = new SliderView();
 const burger = new BurgerView(true);
+
+console.log('Выполнены все пункты задания (даже больше, чем нужно :D)');
