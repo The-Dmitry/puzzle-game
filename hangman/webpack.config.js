fef354644affb48cfbdce0588d8a1f1aeaf70551
the-dmitry <dzmitrygaponenko@gmail.com>
2023-12-30 08:37:39 +0300

    feat: add webpack config, eslint

---
+++ hangman/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = ({ dev }) => ({
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    assetModuleFilename: 'assets/[hash][ext]',
+    clean: true,
+  },
+  performance: {
+    hints: false,
+  },
+  devtool: dev ? 'inline-source-map' : false,
+  mode: dev ? 'development' : 'production',
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ /* title: 'Base', */ template: './src/index.html', filename: 'index.html', minify: dev ? false : true }),
+    new MiniCssExtractPlugin({ /* filename: '[name].[contenthash].css' */ }),
+  ],
+  devServer: {
+    hot: true,
+    open: true,
+    port: 8080,
+    static: './dist',
+  },
+});
