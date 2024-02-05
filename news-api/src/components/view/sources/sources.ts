6bb33ffd29dc24648ba33732f672b8159188f25f
the-dmitry <dzmitrygaponenko@gmail.com>
2024-02-05 22:33:11 +0300

    refactor: integrate typescript to sources.ts

---
+++ news-api/src/components/view/sources/sources.ts
@@ -0,0 +1,22 @@
+import NewsSource from '../../../models/interfaces/NewsSource';
+import './sources.css';
+
+class Sources {
+    draw(data: NewsSource[]) {
+        const fragment = document.createDocumentFragment();
+        const sourceItemTemp = document.querySelector<HTMLTemplateElement>('#sourceItemTemp')!;
+
+        data.forEach((item) => {
+            const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLElement;
+
+            sourceClone.querySelector('.source__item-name')!.textContent = item.name;
+            sourceClone.querySelector('.source__item')!.setAttribute('data-source-id', item.id);
+
+            fragment.append(sourceClone);
+        });
+
+        document.querySelector('.sources')!.append(fragment);
+    }
+}
+
+export default Sources;
