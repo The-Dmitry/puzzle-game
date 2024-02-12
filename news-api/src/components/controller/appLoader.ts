db8421394f52a8e76ac69ddb8640f3bf4f1316b2
the-dmitry <dzmitrygaponenko@gmail.com>
2024-02-13 02:12:21 +0300

    feat: add footer links

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -3,8 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
   constructor() {
     super('https://newsapi.org/v2/', {
-      apiKey: '5aec5d1a5f1d4466b0227a771247f9ef', // получите свой ключ https://newsapi.org/
-      // apiKey: 'cb926c60892c4ad28d14b9fda792e46a', // получите свой ключ https://newsapi.org/
+      apiKey: 'cb926c60892c4ad28d14b9fda792e46a', // получите свой ключ https://newsapi.org/
     });
   }
 }
