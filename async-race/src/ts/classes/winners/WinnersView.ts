e32f0b907ea58a3a3b72e51f7c2fc3dc2af72fae
the-dmitry <dzmitrygaponenko@gmail.com>
2024-03-28 23:55:46 +0300

    feat: add sortin for winners list

--- async-race/src/ts/classes/winners/WinnersView.ts
+++ async-race/src/ts/classes/winners/WinnersView.ts
@@ -25,25 +25,51 @@ export default class WinnersView extends View {
     this.state.subscribe(this.viewCreator, 'updateWinners', () => this.renderGrid());
   }
 
-  private renderGrid() {
-    this.grid.removeAllChildren();
-    this.grid.addInnerNode(this.createControls());
-    this.getWinners();
+  private renderGrid(page: number = 1, sort: WinnersSort = this.sort, order: WinnersOrder = this.order) {
+    this.getWinners(page, sort, order);
   }
 
-  private async getWinners() {
-    const [data, pageCount] = await this.httpClient.getWinnersList();
+  private async getWinners(page: number, sort: WinnersSort, order: WinnersOrder) {
+    const [data, pageCount] = await this.httpClient.getWinnersList(page, sort, order);
     const list = data.map((winner) => this.createInfoRow(winner));
-    this.grid.addInnerNode(...list);
+    this.grid.removeAllChildren();
+    this.grid.addInnerNode(this.createControls(), ...list);
   }
 
   private createControls() {
-    const row = new NodeCreator({ tag: 'div', css: ['winners-grid__row'] });
-    const id = new NodeCreator({ tag: 'button', text: '№' });
-    const image = new NodeCreator({ tag: 'div', text: 'img' });
-    const name = new NodeCreator({ tag: 'div', text: 'name' });
-    const wins = new NodeCreator({ tag: 'button', text: 'wins' });
-    const bestTime = new NodeCreator({ tag: 'button', text: 'bestTime' });
+    const row = new NodeCreator({
+      tag: 'div',
+      css: ['winners-grid__row'],
+    });
+    const id = new NodeCreator({
+      tag: 'button',
+      css: ['control-item'],
+      text: '№',
+      callback: () => this.switchSorting('id'),
+    });
+    if (this.sort === 'id') {
+      id.addClassName(this.order === 'ASC' ? 'control-item_asc' : 'control-item_desc');
+    }
+    const image = new NodeCreator({ tag: 'div', css: ['control-item'] });
+    const name = new NodeCreator({ tag: 'div', text: 'Name' });
+    const wins = new NodeCreator({
+      tag: 'button',
+      css: ['control-item'],
+      text: 'Wins',
+      callback: () => this.switchSorting('wins'),
+    });
+    if (this.sort === 'wins') {
+      wins.addClassName(this.order === 'ASC' ? 'control-item_asc' : 'control-item_desc');
+    }
+    const bestTime = new NodeCreator({
+      tag: 'button',
+      css: ['control-item'],
+      text: 'Best time',
+      callback: () => this.switchSorting('time'),
+    });
+    if (this.sort === 'time') {
+      bestTime.addClassName(this.order === 'ASC' ? 'control-item_asc' : 'control-item_desc');
+    }
     row.addInnerNode(id, image, name, wins, bestTime);
     return row;
   }
@@ -54,7 +80,7 @@ export default class WinnersView extends View {
     const image = new NodeCreator({ tag: 'div', text: 'img' });
     const name = new NodeCreator({ tag: 'div', text: 'name' });
     const winsNum = new NodeCreator({ tag: 'div', text: `${wins}` });
-    const bestTime = new NodeCreator({ tag: 'div', text: `${time}` });
+    const bestTime = new NodeCreator({ tag: 'div', text: `${time.toFixed(2)}` });
     this.getNameAndColor(id, name, image);
     row.addInnerNode(num, image, name, winsNum, bestTime);
     return row;
@@ -64,5 +90,16 @@ export default class WinnersView extends View {
     const { name, color } = await this.httpClient.getOneCar(id);
     nameNode.setTextContent(name);
     imageNode.node.style.setProperty('--car-color', color);
+  }
+
+  private switchSorting(sort: WinnersSort) {
+    if (this.sort === sort) {
+      this.order = this.order === 'ASC' ? 'DESC' : 'ASC';
+      this.renderGrid();
+      return;
+    }
+    this.sort = sort;
+    this.order = 'ASC';
+    this.renderGrid();
   }
 }
